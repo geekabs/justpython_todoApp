a7cb5f98a2d4595b3709f15f436b503f3fd27ca5
TK <ft.0529.mt@gmail.com>
2022-02-03 17:50:58 +0900

    delete heroku info

--- app/settings.py
+++ app/settings.py
@@ -29,7 +29,7 @@ DEBUG = env.get_value('DEBUG', cast = bool)
 if DEBUG:
     ALLOWED_HOSTS = ['*']
 else:
-    ALLOWED_HOSTS = ['tk-todoapp.herokuapp.com']
+    ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
--- todo/models.py
+++ todo/models.py
@@ -15,13 +15,3 @@ class Todo(models.Model):
     
     def __str__(self):
         return self.title
-
-
-# # 投稿記事テーブル
-# class Article(models.Model):
-#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-#     date = models.DateField(verbose_name='日付', default=timezone.now)
-#     title = models.CharField(verbose_name='タイトル', max_length=40)
-#     text = models.CharField(verbose_name='本文', max_length=200)
-#     created_at = models.DateTimeField(verbose_name='作成日時', default=timezone.now)
-#     updated_at = models.DateTimeField(verbose_name='編集日時', blank=True, null=True)
